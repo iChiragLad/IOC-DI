498d4847b4a5f77662ef55f9e18b51b7f84d9035
unknown <chirag_lad@PTD09704.persistent.co.in>
2016-05-05 12:20:04 +0530

    Object Scope

--- Inversion_of_Control/DI.Abstraction/Program.cs
+++ Inversion_of_Control/DI.Abstraction/Program.cs
@@ -17,6 +17,18 @@ namespace DI.Abstraction
             IKernel kernel = new StandardKernel(new Registration());
             Commerce commerce = kernel.Get<Commerce>();
             commerce.ProcessOrder(new OrderInfo());
+
+            Console.WriteLine("   ");
+
+            FileLogger log1 =(FileLogger) kernel.Get<ILogger>();
+            FileLogger log2 = (FileLogger)kernel.Get<ILogger>();
+
+            Console.WriteLine(log1.Equals(log2));
+
+            EmailNotification not1 = (EmailNotification)kernel.Get<INotifier>();
+            EmailNotification not2 = (EmailNotification)kernel.Get<INotifier>();
+
+            Console.WriteLine(not1.Equals(not2));
             Console.ReadKey();
         }
     }
