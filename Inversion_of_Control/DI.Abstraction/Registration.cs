885babdfb4f7e04aa1039d2187ca5b2da1cc654e
Chirag Lad <chiragladn92@gmail.com>
2016-05-07 13:37:45 +0530

    Complex initialization using ToProvider()

--- Inversion_of_Control/DI.Abstraction/Registration.cs
+++ Inversion_of_Control/DI.Abstraction/Registration.cs
@@ -1,5 +1,6 @@
 ﻿using DI.Abstraction.Dependencies;
 using DI.Abstraction.Dependency.Interfaces;
+using Ninject.Activation;
 using Ninject.Modules;
 using System;
 using System.Collections.Generic;
@@ -16,11 +17,21 @@ namespace DI.Abstraction
             Bind<IBillingProcessor>().To<BillingProcessor>();
             Bind<IDelivery>().To<AirShipment>().WhenTargetHas(typeof(AirAttribute));
             Bind<IDelivery>().To<RailShipment>().WhenTargetHas(typeof(RailAttribute));
-            Bind<ILogger>().To<FileLogger>();
+
+            Bind<ILogger>().ToProvider(new FileLoggerProvider());
+
             Bind<INotifier>().To<EmailNotification>();
             Bind<ITaxCalculator>().To<TaxCalculator>();
             Bind<IMath>().To<Dependencies.Math>();
             Bind<Commerce>().ToSelf().WithConstructorArgument("x", 10).WithConstructorArgument("y", "Hello World");
+        }
+    }
+    class FileLoggerProvider : Provider<ILogger>
+    {
+        protected override ILogger CreateInstance(IContext context)
+        {
+            //Do some complex initialization of FileLogger like fetching from Configuration file, connection string for database objects etc..
+            return new FileLogger();
         }
     }
     public class AirAttribute : Attribute
